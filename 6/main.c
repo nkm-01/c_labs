814914cec1e67469220aebc26ae63b59dd2555c8
Мацюк Никита <matsuk-nikita@rambler.ru>
2022-11-03 18:28:26 +0500

    lab 6 done

--- 6/main.c
+++ 6/main.c
@@ -6,7 +6,7 @@ void fill_random(int n, int a[])
 {
     int i;
     for (i = 0; i < n; i++)
-        a[i] = rand () % 101 - 50;
+        a[i] = rand () % 20 - 10;
 }
 
 int find_same_element(int from, int a[], int array_size)
@@ -36,9 +36,22 @@ int find_product(int from, int to, int a[])
     return product;
 }
 
-int print_array()
+int print_array(int size, int a[])
 {
+    printf("       "); // 7 пробелов
+    for (int i = 0; i <= 9; i++)
+        printf("%6d ", i);
+    printf("\n");
 
+    for (int l = 0; l <= size/10; l++)
+    {
+        if (l * 10 >= size)
+            break;
+        printf("%6d ", l);
+        for (int i = l * 10; i < size && i < (l + 1) * 10; i++)
+            printf("%6d ", a[i]);
+        printf("\n");
+    }
 }
 
 int main()
@@ -54,20 +67,32 @@ int main()
     printf("Массив до:\n");
     print_array(array_size, a);
 
+    int sum, product, begin_number, same_number = -1;
     for (int i = 0; i < array_size; i++)
     {
-        int same_number = find_same_element(i, a, array_size);
+        same_number = find_same_element(i, a, array_size);
         if (same_number != -1)
         {
-            int sum = find_sum(i + 1, same_number - 1, a);
-            int product = find_product(i + 1, same_number - 1, a);
+            sum = find_sum(i + 1, same_number - 1, a);
+            product = find_product(i + 1, same_number - 1, a);
 
             a[i] = sum;
             a[same_number] = product;
+            begin_number = i;
             break;
         }
     }
 
     printf("Массив после:\n");
     print_array(array_size, a);
+
+    if (same_number != -1)
+    {
+        printf("Одинаковые элементы № %d и %d.\nСумма элементов между ними: %d, произведение: %d\n",
+            begin_number, same_number, sum, product);
+    }
+    else
+        printf("Одинаковые элементы не найдены.\n");
+
+    return 0;
 }
